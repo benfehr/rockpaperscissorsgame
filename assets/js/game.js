f5ea5f47672f8dcc371fb807af3ad667d9759043
benfehr <benjamin_louis_foote@hotmail.com>
2023-01-23 20:34:44 +0000

    create const for buttons playerscore computer score and reset button

---
+++ assets/js/game.js
@@ -0,0 +1,4 @@
+const buttons = Array.from(document.getElementsByClassName('btn'));
+const playerScoreElement = document.getElementById('playerScore');
+const computerScoreElement = document.getElementById('computerScore');
+const resetButton = document.getElementById('reset');
