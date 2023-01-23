aca89603d900b76e0897d35007a6c488339062bb
benfehr <benjamin_louis_foote@hotmail.com>
2023-01-23 20:49:56 +0000

    create button event listener

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,3 +2,38 @@ const buttons = Array.from(document.getElementsByClassName('btn'));
 const playerScoreElement = document.getElementById('playerScore');
 const computerScoreElement = document.getElementById('computerScore');
 const resetButton = document.getElementById('reset');
+
+const hands = [
+    {
+        name: 'rock',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        beats: 'paper'
+    },
+]
+
+let playerScore = 0;
+let computerScore = 0;
+let gameOverState= false;
+
+for (button of buttons) {
+    button.addEventListener('click', (e) => {
+        if (!gameOverState) {
+            playRound(e.target.dataset['hand'])
+        } else {
+            Swal.fire({
+                position: 'center',
+                icon: 'info',
+                title: "Game over, reset to play again",
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+    })
+}
